065ae964052f68a8ca246168d48a3d1deeb9c2e7
Daniel Ramirez <daniel.ramirzc22@gmail.com>
2024-01-15 11:53:20 -0600

    Add project files.

---
+++ MathGameMaui/GamePage.xaml.cs
@@ -0,0 +1,106 @@
+using MathGameMaui.Models;
+
+namespace MathGameMaui;
+
+public partial class GamePage : ContentPage
+{
+    public string GameType { get; set; }
+	int firstNumber = 0;
+	int secondNumber = 0;
+	int score = 0;
+	const int totalQuestions = 2;
+	int gamesLeft = totalQuestions;
+
+    public GamePage(string gameType)
+	{
+		InitializeComponent();
+		GameType = gameType;
+		BindingContext = this;
+
+		CreateNewQuestion();
+	}
+
+	private void CreateNewQuestion()
+	{
+		Random random = new Random();
+		firstNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+		firstNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+
+
+		if (GameType == "Division")
+		{
+			while(firstNumber < secondNumber || firstNumber % secondNumber != 0) 
+			{
+				firstNumber = random.Next(1, 99);
+				secondNumber = random.Next(1, 99);
+			}
+		}
+
+		QuestionLabel.Text = $"{firstNumber} {GameType} {secondNumber}";
+	}
+	private void OnAnswerSubmitted(object sender, EventArgs e)
+	{
+		int answer = Int32.Parse(AnswerEntry.Text);
+		bool isCorrect = false;
+
+		switch (GameType)
+		{
+			case "+":
+				isCorrect = answer == firstNumber + secondNumber;
+				break;
+			case "-":
+                isCorrect = answer == firstNumber - secondNumber;
+                break;
+			case "×":
+                isCorrect = answer == firstNumber * secondNumber;
+                break;
+			case "÷":
+                isCorrect = answer == firstNumber / secondNumber;
+                break;
+        }
+
+        ProcessAnswer(isCorrect);
+
+		gamesLeft--;
+		AnswerEntry.Text = "";
+
+		if (gamesLeft > 0)
+			CreateNewQuestion();
+		else
+			GameOver();
+    }
+
+	private void GameOver()
+	{
+		GameOperation gameOperation = GameType switch
+		{
+			"+" => GameOperation.Addition,
+			"-" => GameOperation.Subtraction,
+            "×" => GameOperation.Multiplication,
+            "÷" => GameOperation.Division
+		};
+
+		QuestionArea.IsVisible = false;
+		BackToMenuBtn.IsVisible = true;
+		GameOverLabel.Text = $"Game Over! You got {score} out of {totalQuestions} right";
+
+		App.GameRepository.Add(new Game
+		{
+			DatePlayed = DateTime.Now,
+			Type = gameOperation,
+			Score = score
+		});
+	}
+
+	private void ProcessAnswer(bool isCorrect)
+	{
+		if (isCorrect) score += 1;
+
+		AnswerLabel.Text = isCorrect ? "Correct!" : "Incorrect";
+	}
+
+	private void OnBackToMenu(object sender, EventArgs e)
+	{
+		Navigation.PushAsync(new MainPage());
+	}
+}
---
+++ MathGameMaui/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using MathGameMaui.Data;
+using Microsoft.Extensions.Logging;
+
+namespace MathGameMaui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Outfit-VariableFont_wght.ttf", "Outfit_VariableFont_regular");
+                });
+
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "game.db");
+
+            builder.Services.AddSingleton(s =>
+                ActivatorUtilities.CreateInstance<GameRepository>(s, dbPath));
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
